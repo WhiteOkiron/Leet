0408df52bb5bfe385c2989314dd5fb167dd6988f
WhiteOkiron <whiteokiron@hotmail.com>
2018-09-22 12:43:02 -0700

    Created a function getMinLetterCount() to count the vector of strings

--- Solution14/Solution14/Solution14.cpp
+++ Solution14/Solution14/Solution14.cpp
@@ -18,17 +18,62 @@
 #include <vector>
 using namespace std;
 
+
+int minimum = 0;
 class Solution {
+
 public:
 	string longestCommonPrefix(vector<string>& strs) {
 
+		int minSize = getMinSize(strs);
+		for (int i = 0; i < minSize; i++)
+		{
+
+		}
 	}
+
 };
 
+int getMinSize(vector<string>& strs)
+{
+	minimum = strs[0].length();
+	for (int i = 0; i < strs.size(); i++)
+	{
+		if (strs[i].length() < minimum)
+		{
+			minimum = strs[i].length();
+		}
+	}
+	return minimum;
+}
+
+int test()
+{
+	vector<string>word = { "onesdf", "two", "three", "four", "fives" };
+
+	//cout << word[0] << endl;       //! one
+	//cout << word[0][1] << endl;    //! n
+	//int size = static_cast<int>(word.size());
+	//cout << size << endl;
+	//cout << word.size() << endl;
+	int MinSize = getMinSize(word);
+
+
+
+
+
+
+	return 0;
+}
 
 int main()
 {
+	//! Find the length of the shortest word
+	//! Loop through each letters in all the words using that length
 
+	test();
+	return 0;
 
 }
+
 
