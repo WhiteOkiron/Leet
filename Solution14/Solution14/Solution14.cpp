eef1f9771fb5f75d87f013805944c491bb4eedf9
WhiteOkiron <whiteokiron@hotmail.com>
2018-09-22 00:33:52 -0700

    Initial Commit

---
+++ Solution14/Solution14/Solution14.cpp
@@ -0,0 +1,34 @@
+//TODO: Write a function to find the longest common prefix string amongst an array of strings.
+
+//TODO: If there is no common prefix, return an empty string "".
+
+//TODO: Example 1:
+
+//TODO: Input: ["flower", "flow", "flight"]
+//TODO: 	Output : "fl"
+//TODO: 	Example 2 :
+
+//TODO: 	Input : ["dog", "racecar", "car"]
+//TODO: 	Output : ""
+//TODO: 	Explanation : There is no common prefix among the input strings.
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	string longestCommonPrefix(vector<string>& strs) {
+
+	}
+};
+
+
+int main()
+{
+
+
+}
+
